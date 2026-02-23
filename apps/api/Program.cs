eb2b412f965a21e3f358f5ddf22c225ba17c2da8
Valentin Negru <nevalenti@proton.me>
2026-02-23 22:03:14 +0200

    feat(api): add cors policy for angular dev server

--- apps/api/Program.cs
+++ apps/api/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader()));
 
 var app = builder.Build();
 
@@ -9,6 +14,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello, World!");
