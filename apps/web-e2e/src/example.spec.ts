4e5c58581a9fa708ea4f6122494520d940361a9f
Valentin Negru <nevalenti@proton.me>
2026-03-01 13:33:48 +0200

    fix(test): fix e2e title test

--- apps/web-e2e/src/example.spec.ts
+++ apps/web-e2e/src/example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page).toHaveTitle('web');
+  await expect(page).toHaveTitle('Better Tests');
 });
